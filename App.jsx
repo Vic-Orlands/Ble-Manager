64db773ecd24c5f98d9b9400d71235d0ef1774fb
Vic.Orlands <chimezieinnocent39@gmail.com>
2023-06-30 12:25:47 +0100

    committed all files

---
+++ App.jsx
@@ -0,0 +1,337 @@
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react-native/no-inline-styles */
+
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  Alert,
+  View,
+  Platform,
+  StatusBar,
+  ScrollView,
+  StyleSheet,
+  SafeAreaView,
+  NativeModules,
+  useColorScheme,
+  TouchableOpacity,
+  NativeEventEmitter,
+  PermissionsAndroid,
+} from 'react-native';
+import BleManager from 'react-native-ble-manager';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+const BleManagerModule = NativeModules.BleManager;
+const BleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+
+const App = () => {
+  const peripherals = new Map();
+  const [isScanning, setIsScanning] = useState(false);
+  const [connectedDevices, setConnectedDevices] = useState([]);
+  const [discoveredDevices, setDiscoveredDevices] = useState([]);
+
+  const handleGetConnectedDevices = () => {
+    BleManager.getBondedPeripherals([]).then(results => {
+      // Each peripheral in returned array will have id and name properties
+      console.log('Bonded peripherals: ' + results.length);
+
+      for (let i = 0; i < results.length; i++) {
+        let peripheral = results[i];
+        peripheral.connected = true;
+        peripherals.set(peripheral.id, peripheral);
+        setConnectedDevices(Array.from(peripherals.values()));
+      }
+    });
+
+    BleManager.getConnectedPeripherals([]).then(results => {
+      if (results.length === 0) {
+        console.log('No connected bluetooth devices');
+      } else {
+        for (let i = 0; i < results.length; i++) {
+          let peripheral = results[i];
+          peripheral.connected = true;
+          peripherals.set(peripheral.id, peripheral);
+          setConnectedDevices(Array.from(peripherals.values()));
+        }
+      }
+    });
+  };
+
+  useEffect(() => {
+    BleManager.enableBluetooth().then(() => {
+      console.log('Bluetooth is turned on!');
+    });
+
+    BleManager.start({showAlert: false}).then(() => {
+      console.log('BleManager initialized');
+      handleGetConnectedDevices();
+    });
+
+    let stopDiscoverListener = BleManagerEmitter.addListener(
+      'BleManagerDiscoverPeripheral',
+      peripheral => {
+        peripherals.set(peripheral.id, peripheral);
+        setDiscoveredDevices(Array.from(peripherals.values()));
+      },
+    );
+
+    let stopConnectListener = BleManagerEmitter.addListener(
+      'BleManagerConnectPeripheral',
+      peripheral => {
+        console.log('BleManagerConnectPeripheral:', peripheral);
+      },
+    );
+
+    let stopScanListener = BleManagerEmitter.addListener(
+      'BleManagerStopScan',
+      () => {
+        setIsScanning(false);
+        console.log('scan stopped');
+      },
+    );
+
+    if (Platform.OS === 'android' && Platform.Version >= 23) {
+      PermissionsAndroid.check(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      ).then(result => {
+        if (result) {
+          console.log('Permission is OK');
+        } else {
+          PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          ).then(result => {
+            if (result) {
+              console.log('User accepted');
+            } else {
+              console.log('User refused');
+            }
+          });
+        }
+      });
+    }
+
+    return () => {
+      stopDiscoverListener.remove();
+      stopConnectListener.remove();
+      stopScanListener.remove();
+    };
+  }, []);
+
+  const startScan = () => {
+    if (!isScanning) {
+      BleManager.scan([], 5, true)
+        .then(() => {
+          console.log('Scanning...');
+          setIsScanning(true);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  };
+
+  // pair with device first before connecting to it
+  const connectToPeripheral = peripheral => {
+    BleManager.createBond(peripheral.id)
+      .then(() => {
+        console.log('BLE device paired successfully');
+        pairSuccessful(peripheral);
+      })
+      .catch(() => {
+        console.log('failed to bond');
+      });
+  };
+
+  const pairSuccessful = peripheral => {
+    BleManager.connect(peripheral.id)
+      .then(() => {
+        peripheral.connected = true;
+        peripherals.set(peripheral.id, peripheral);
+        setConnectedDevices(Array.from(peripherals.values()));
+        setDiscoveredDevices(Array.from(peripherals.values()));
+        console.log(`Connected to ${peripheral.name}`);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  // disconnect from device
+  const disconnectFromPeripheral = peripheral => {
+    // BleManager.disconnect(peripheral.id).then(() => {
+
+    BleManager.removeBond(peripheral.id)
+      .then(() => {
+        peripheral.connected = false;
+        peripherals.set(peripheral.id, peripheral);
+        // setConnectedDevices(Array.from(peripherals.values()));
+        setDiscoveredDevices(Array.from(peripherals.values()));
+        Alert.alert(`Disconnected from ${peripheral.name}`);
+        // console.log('removeBond success');
+      })
+      .catch(() => {
+        console.log('fail to remove the bond');
+      });
+    // });
+  };
+
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  // render list of bluetooth devices
+  const RenderItem = ({peripheral}) => {
+    const {name, rssi, connected} = peripheral;
+
+    return (
+      <>
+        {name && (
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              marginBottom: 10,
+            }}>
+            <View style={styles.deviceItem}>
+              <Text style={styles.deviceName}>{name}</Text>
+              <Text style={styles.deviceInfo}>RSSI: {rssi}</Text>
+            </View>
+
+            <TouchableOpacity
+              onPress={() =>
+                connected
+                  ? disconnectFromPeripheral(peripheral)
+                  : connectToPeripheral(peripheral)
+              }
+              style={styles.deviceButton}>
+              <Text
+                style={[
+                  styles.scanButtonText,
+                  {fontWeight: 'bold', fontSize: 16},
+                ]}>
+                {connected ? 'Disconnect' : 'Connect'}
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </>
+    );
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        style={backgroundStyle}
+        contentContainerStyle={styles.scrollContainer}
+        contentInsetAdjustmentBehavior="automatic">
+        <Text
+          style={[
+            styles.title,
+            {color: isDarkMode ? Colors.white : Colors.black},
+          ]}>
+          React Native BLE Manager Tutorial
+        </Text>
+        <TouchableOpacity
+          activeOpacity={0.5}
+          style={styles.scanButton}
+          onPress={startScan}>
+          <Text style={styles.scanButtonText}>
+            {isScanning ? 'Scanning...' : 'Scan Bluetooth Devices'}
+          </Text>
+        </TouchableOpacity>
+
+        <Text
+          style={[
+            styles.subtitle,
+            {color: isDarkMode ? Colors.white : Colors.black},
+          ]}>
+          Discovered Devices:
+        </Text>
+        {discoveredDevices.length > 0 ? (
+          discoveredDevices.map(device => (
+            <React.Fragment key={device.id}>
+              <RenderItem peripheral={device} />
+            </React.Fragment>
+          ))
+        ) : (
+          <Text style={styles.noDevicesText}>No Bluetooth devices found</Text>
+        )}
+
+        <Text
+          style={[
+            styles.subtitle,
+            {color: isDarkMode ? Colors.white : Colors.black},
+          ]}>
+          Connected Devices:
+        </Text>
+        {connectedDevices.length > 0 ? (
+          connectedDevices.map(device => (
+            <React.Fragment key={device.id}>
+              <RenderItem peripheral={device} />
+            </React.Fragment>
+          ))
+        ) : (
+          <Text style={styles.noDevicesText}>No connected devices</Text>
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollContainer: {
+    padding: 16,
+  },
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+    marginBottom: 20,
+    marginTop: 40,
+  },
+  subtitle: {
+    fontSize: 24,
+    marginBottom: 10,
+    marginTop: 20,
+  },
+  scanButton: {
+    backgroundColor: '#2196F3',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  scanButtonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  deviceItem: {
+    marginBottom: 10,
+  },
+  deviceName: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
+  deviceInfo: {
+    fontSize: 14,
+  },
+  noDevicesText: {
+    textAlign: 'center',
+    marginTop: 10,
+    fontStyle: 'italic',
+  },
+  deviceButton: {
+    backgroundColor: '#2196F3',
+    padding: 8,
+    borderRadius: 5,
+    marginBottom: 20,
+    paddingHorizontal: 20,
+  },
+});
+
+export default App;
